fb7cdeb775702133ae5443845f2a81b8a1aa331a
Chris Nady <nady.christopher@gmail.com>
2017-08-26 02:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   } else if (cart.length === 1) {
     console.log("In your cart, you have " + Object.keys(cart[0])[0] + " at $" + cart[0][Object.keys(cart[0])[0]] + ".");
   } else if (cart.length ===2 ) {
-    console.log("In your cart, you have " + cart[0] + " at $" + cart[0] + " and " + cart [1] + " at $" + cart[1] + ".");
+    console.log("In your cart, you have " + Object.keys(cart[0])[0]  + " at $" + cart[0][Object.keys(cart[0])[0]]  + " and " + Object.keys(cart[1])[0]  + " at $" + cart[1][Object.keys(cart[1])[0]]  + ".");
   } else {
     console.log("In your cart, you have " + cart[0] + " at $" + cart[0] + ", " + cart [1] + " at $" + cart[1] + ", and " + cart [2] + " at $" + cart[2] + ".");
   }
